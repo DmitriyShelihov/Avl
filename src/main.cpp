96c082bbacbc8db94ad46bd49733ccbbbf22402a
Mihail Ilyushenkov <iliushenkov.mv@phystech.edu>
2024-11-23 14:36:45 +0300

    added main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-#include <random>
-
-void print_elem(int elem) {
-	std::cout << elem;
-}
-
+#include <set>
 #include "avl_tree.h"
 
 int compare(int a, int b) {
@@ -15,46 +10,32 @@ int compare(int a, int b) {
 	return -1;
 }
 
+int main(){
+    Avl<int> avl(compare);
+    int val1 = 0;
+    int val2 = 0;
+    char command = '\0';
+    
+    while(std::cin.good()){
+        std::cin >> command;
 
-int main() {
+        if(command == 'k'){
+            std::cin >> val1;
+            avl.insert(val1);
+        }
+        else if(command == 'q'){
+            std::cin >> val1;
+            std::cin >> val2;
 
-	std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist100(1,100); // distribution in range [1, 6]
-
-	for(int l = 0; l < 10; ++l){
-		for(int k = 0; k < 1; k++){
-			Avl<int> avl(compare);
-
-			int min = dist100(rng);
-			int max = dist100(rng);
-
-			if(min > max){
-				int tmp = min;
-				min = max;
-				max = tmp;
-			}
-
-			int counter = 0;
-			int val = 0;
-			for(int i = 0; i < 1000; i ++){
-				val = dist100(rng);
-				avl.insert(val);
-			}
-
-			for(auto x = avl.begin(); x != avl.end(); x++){
-				if(min <= (*x) && (*x)<= max) counter++;
-			}
-			int res = avl.get_count(min, max);
-
-			if(res != counter){
-				avl.dump();
-				std::cout << "found " << res << " elements between " << min << " and " << max << " (real counter: "<< counter << ")\n";
-			}
-			else { 
-				std::cout << "passed_in_main\n";
-			}
-		}
-	}
-	return 0;
+            if(val1 > val2){
+                std::cout << 0 << ' ';
+            }
+            else {
+                std::cout << avl.get_count(val1, val2) << ' ';
+            }
+        }
+        else {
+            return 0;
+        }
+    }
 }
